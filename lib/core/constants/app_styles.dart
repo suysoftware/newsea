918b55fef9a69179b7ef14bc81bb87c75f52824a
SUY <suy.register@gmail.com>
2023-09-09 14:21:27 +0300

    App Styles

---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+
+class AppStyles {
+
+
+
+//body
+  static const TextStyle bodyBlack = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w200,
+      fontSize: 13.0,
+      // height: 13.0,
+      color: AppColors.black);
+  static const TextStyle bodyWhite = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w200,
+      fontSize: 13.0,
+      // height: 13.0,
+      color: AppColors.white);
+        static const TextStyle bodyGreen = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w200,
+      fontSize: 13.0,
+      // height: 13.0,
+      color: AppColors.green);
+        static const TextStyle bodyGrey = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w200,
+      fontSize: 13.0,
+      // height: 13.0,
+      color: AppColors.grey);
+
+
+
+
+//headline
+        static const TextStyle headlineBlack = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 15.0,
+      // height: 13.0,
+      color: AppColors.black);
+
+     static const TextStyle headlineWhite = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 15.0,
+      // height: 13.0,
+      color: AppColors.white);
+
+      static const TextStyle headlineGreen = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 15.0,
+      // height: 13.0,
+      color: AppColors.green);
+
+       static const TextStyle headlineGrey = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 15.0,
+      // height: 13.0,
+      color: AppColors.grey);
+
+
+
+//caption
+        static const TextStyle captionBlack = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w100,
+      fontSize: 8.0,
+      // height: 13.0,
+      color: AppColors.black);
+
+     static const TextStyle captionWhite = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w100,
+      fontSize: 8.0,
+      // height: 13.0,
+      color: AppColors.white);
+        static const TextStyle captionGreen = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w100,
+      fontSize: 8.0,
+      // height: 13.0,
+      color: AppColors.green);
+
+     static const TextStyle captionGrey = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w100,
+      fontSize: 8.0,
+      // height: 13.0,
+      color: AppColors.grey);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ////////
+  static const TextStyle readListStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 11.0,
+      // height: 13.0,
+      color: AppColors.themeDarkBlue);
+
+  static const TextStyle dateStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 12.0,
+      //height: 15.0,
+      color: AppColors.textLightBlue);
+
+  static const TextStyle mainScreenTitleStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w700,
+      fontSize: 18.0,
+      //  height: 22.0,
+      color: AppColors.feedTitleColor);
+
+  static const TextStyle mainScreenBodyStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 12.0,
+      //  height: 14.52,
+      color: AppColors.textLightBlue);
+
+  static const TextStyle mainScreenCategoryButtonActiveTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w600,
+      fontSize: 14.0,
+      // height: 16.94,
+      color: AppColors.themeWhite);
+
+  static const TextStyle mainScreenCategoryButtonPassiveTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w600,
+      fontSize: 14.0,
+      //height: 16.94,
+      color: AppColors.themeDarkBlue);
+
+  static const TextStyle sourceScreenTitleStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w700,
+      fontSize: 16.0,
+      //height: 19.36,
+      color: AppColors.themeDarkBlue);
+
+  static const TextStyle sourceScreenBodyStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 12.0,
+      // height: 14.52,
+      color: AppColors.textLightBlue);
+
+  static const TextStyle sourceScreenReadListButtonTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 11.0,
+      //height: 13.31,
+      color: AppColors.themeDarkBlue);
+
+  static const TextStyle sourceScreenCategoryTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w700,
+      fontSize: 22.0,
+      //height: 26.63,
+      color: AppColors.softBlack);
+
+  static const TextStyle sourceScreenBackButtonTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w600,
+      fontSize: 12.0,
+      //  height: 14.52,
+      color: AppColors.themeWhite);
+
+  static const TextStyle detailScreenTitleStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w700,
+      fontSize: 24.0,
+      //  height: 29.05,
+      color: AppColors.themeDarkBlue);
+
+  static const TextStyle detailScreenBodyStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w400,
+      fontSize: 12.0,
+      //  height: 14.52,
+      color: AppColors.textLightBlue);
+
+  static const TextStyle detailScreenBackButtonTextStyle = TextStyle(
+      fontFamily: "Inter-Regular",
+      fontWeight: FontWeight.w600,
+      fontSize: 14.0,
+      // height: 16.94,
+      color: AppColors.themeWhite);
+}
