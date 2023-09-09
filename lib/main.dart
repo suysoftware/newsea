5020f553be30dc0564f15bc3f89046389b6fed54
SUY <suy.register@gmail.com>
2023-09-09 07:44:14 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(),
+    );
+  }
+}
