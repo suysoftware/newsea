05291c3cae34e538a8d8ad12198ca6501dbe25e1
SUY <suy.register@gmail.com>
2023-09-09 20:51:28 +0300

    FeedList

--- lib/feature/screens/home/presentation/widgets/feed_list.dart
+++ lib/feature/screens/home/presentation/widgets/feed_list.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:newsea/core/constants/app_colors.dart';
+import 'package:newsea/core/models/feed_model.dart';
 import 'package:newsea/feature/screens/home/controller/home_controller.dart';
-
 
 import 'feed_card.dart';
 
@@ -14,34 +15,88 @@ class FeedList extends StatefulWidget {
 }
 
 class _FeedListState extends State<FeedList> {
+  final HomeController homeController = Get.find();
   @override
   Widget build(BuildContext context) {
     return GetBuilder(
         init: HomeController(),
         builder: (homeController) {
-
- return Flexible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Container(
-                  decoration: const BoxDecoration(border: Border(top: BorderSide(width: 1.0, color: AppColors.mainListBorderStroke))),
-                  child: ListView.builder(
-                      reverse: false,
-                      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.values.last,
-                      itemCount: homeController.feeds.length,
-                      itemBuilder: (context, index) {
-                        return homeController.feeds.isNotEmpty ? FeedCard(feed: homeController.feeds[index]) : const SizedBox();
-                      }),
-                ),
-              ),
+          if (homeController.nsUser.userLanguage == "") {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
+          }
+          print(homeController.categories);
+          return StreamBuilder<QuerySnapshot<FeedModel>>(
+              stream: FirebaseFirestore.instance
+                  .collection("translated_feeds")
+                  .doc(homeController.nsUser.userLanguage)
+                  .collection("feeds")
+                  .withConverter(fromFirestore: (snapshot, _) => FeedModel.fromJson(snapshot.data()!), toFirestore: (feed, _) => feed.toJson())
+                  .orderBy("publishedAt", descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  var dataCome = convert(snapshot.requireData.docs.map((e) => e.data()).toList(), homeController.categories, homeController.countries);
 
-    
-
-
-
-
-
-    });
+                  return Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Container(
+                        decoration: const BoxDecoration(border: Border(top: BorderSide(width: 1.0, color: AppColors.mainListBorderStroke))),
+                        child: ListView.builder(
+                            reverse: false,
+                            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.values.last,
+                            itemCount: dataCome.length,
+                            itemBuilder: (context, index) {
+                              return dataCome.isNotEmpty ? FeedCard(feed: dataCome[index]) : const SizedBox();
+                            }),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              });
+        });
   }
+}
+
+convert(List<FeedModel> feeds, List<String> categories, List<String> countries) {
+  List<FeedModel> categoryFilteredFeeds = [];
+  List<FeedModel> filteredFeeds = [];
+
+  if (categories.contains("all")) {
+    return feeds;
+  }
+  for (var feed in feeds) {
+    for (var category in categories) {
+      if (feed.categories!.contains(category.capitalizeFirst)) {
+        categoryFilteredFeeds.add(feed);
+      }
+    }
+  }
+
+  if (countries.contains("all")) {
+    return categoryFilteredFeeds;
+  }
+
+for (var feed in categoryFilteredFeeds) {
+    for (var country in countries) {
+      if (feed.country==country.capitalizeFirst) {
+        filteredFeeds.add(feed);
+      }
+    }
+  }
+
+
+
+  return filteredFeeds;
 }
