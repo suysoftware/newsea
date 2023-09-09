e601a3580deb64b1ad2563a56cc6f2d70945441b
SUY <suy.register@gmail.com>
2023-09-09 14:21:23 +0300

    App Colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColors {
+//
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
+  static const Color green = Color.fromARGB(255, 25, 154, 141);
+  static const Color grey = Color.fromARGB(255, 195, 195, 195);
+
+  static const Color themeDarkBlue = Color(0xff052C54);
+  static const Color textLightBlue = Color(0xff7C89A0);
+  static const Color textGrey = Color(0xffDBE5FF);
+  static const Color themeWhite = Color(0xffFFFFFF);
+  static const Color mainListBorderStroke = Color(0xffDBE5FF);
+  static const Color feedTitleColor = Color(0xff5325EA);
+  static const Color softBlack = Color(0xff000000);
+}
